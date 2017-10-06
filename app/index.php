0c5aaa321c73911a8d0d8500daf2e2c24b1ab474
Clavos <clabaq@gmail.com>
2017-10-06 15:00:47 +0200

    Modification du docker compose

--- app/index.php
+++ app/index.php
@@ -1,20 +1,7 @@
-<?php
- function validate(){
-	 echo "Utilisateur est enregistré"
- }
-?>
-
 <html>
 	<head>
 		<title>Mon application PHP 2</title>
 	</head>
 	<body>
-		<form method="POST">
-			Nom : <br>
-			<input type="text"/><br>
-			Prenom : <br>
-			<input type="mdp"/><br>
-			<button onClick="validate">Valider</button>
-		</form>
 	</body>
 </html>
